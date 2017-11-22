12aa14178ee0b30ef56aa03704b7e41f5c0aa38e
Marcos Venezuela <marcosvenezuela.araujo@gmail.com>
2017-11-21 23:16:15 -0200

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { rootRoute } from '@angular/router/src/router_module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { TopMenuComponent } from './navigation/top-menu/top-menu.component';
+import { MenuHeaderComponent } from './navigation/menu-header/menu-header.component';
 
+//config
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    TopMenuComponent,
+    MenuHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig, {useHash:false})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/menu-header/menu-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-header',
+  templateUrl: './menu-header.component.html',
+  styles: []
+})
+export class MenuHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
